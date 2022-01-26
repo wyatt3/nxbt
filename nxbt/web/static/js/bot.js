dbfdbb0895328a42c49f39a59ca9c7e4296ad60a
Wyatt Johnson <wyatt.j1834@gmail.com>
2022-01-25 17:46:03 -0700

    set up auto macro trigger

--- nxbt/web/static/js/bot.js
+++ nxbt/web/static/js/bot.js
@@ -1,6 +1,8 @@
-const macro = `` //Put your macro here
-
-document.addEventListener("DOMContentLoaded", function(e) {
-    // restartController();
-    setTimeout(sendMacro(macro), 5000);
-});
+const macro = `
+HOME 0.1s
+1.5s
+HOME 0.1s
+0.1s
+` //Put your macro here
+createProController();
+setTimeout(() => {sendMacro(macro)}, 6500);
